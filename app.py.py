1089ba417f573da1a176beb4fcb918719fafb39e
RhuleCode <nkbrhule@gmail.com>
2026-05-21 04:15:07 +0000

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -95,7 +95,11 @@ with st.sidebar:
         """)
         # Link button directly pointing to your main GitHub account page
         st.link_button("🌐 View GitHub Profile", "https://github.com/Rhulecode")
+        
+    st.container(height=350, border=False)
+    
     st.divider()
+    
     
     # 1. Log Out Logic
     if st.button("Log Out"):
